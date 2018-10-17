1bee60e56770d8518c7b292e28ee4a991c583d6b
htcr <congruihetang@gmail.com>
2018-10-16 23:14:48 -0400

    finished euler, untested

--- euler.py
+++ euler.py
@@ -5,8 +5,21 @@ def euler(start, end, h, f, y_start):
     # the interval of target function
     # h: real number, iteration step,
     # h*(end-start) > 0
-    # f: dy/dx = f(x, y)
+    # f: dy/dx = f(x, y) 
+    # lambda expression with 2 args
     # y_start: real number, y_start
+    # return: list of tuples [(x0, y0)...]
+
     ans = list()
+
+    assert h*(end-start) > 0
+
+    cur_y = y_start
+    cur_x = start
     
+    while cur_y <= end:
+        ans.append((cur_x, cur_y))
+        cur_x += h
+        cur_y += h*f(cur_x, cur_y)
     
+    return ans    
