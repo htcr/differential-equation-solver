8ac4796047afba597d62daf89c7b087a14800e24
htcr <congruihetang@gmail.com>
2018-10-21 18:18:19 -0400

    finished euler

--- euler.py
+++ euler.py
@@ -17,9 +17,38 @@ def euler(start, end, h, f, y_start):
     cur_y = y_start
     cur_x = start
     
-    while cur_y <= end:
+    iter_num = int((end-start)/h)
+
+    for i in range(iter_num):
         ans.append((cur_x, cur_y))
-        cur_x += h
         cur_y += h*f(cur_x, cur_y)
+        cur_x += h
+
+    ans.append((cur_x, cur_y))
     
     return ans    
+
+fxy = lambda x, y: 1.0 / 3*(y**2)
+
+fx = lambda x: (x-1)**(1.0/3)
+
+ans = euler(2, 1, -0.05, fxy, 1)
+
+print('x        fx_true  fx_euler error')
+
+for xy in ans:
+    try:
+        x = xy[0]
+        x = 1 if x<1 else x
+        fx_euler = xy[1]
+        fx_gt = fx(x)
+        #print('y(%f) = %f' % (xy[0], xy[1]))
+        #print('error: %f' % (fx_euler - fx_gt))
+        print('%.6f %.6f %.6f %.6f' % (x, fx_gt, fx_euler, fx_gt - fx_euler))    
+
+    except Exception:
+        print(x)
+    
+
+
+    
